96a0b0c9beecea511eca5364f7a21038bd5653ed
Galkina-Darya <99248872+Galkina-Darya@users.noreply.github.com>
2022-10-31 13:43:44 +0300

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,27 @@
+<?php 
+$title="Форма регистрации"; // название формы
+require __DIR__ . '/header.php'; // подключаем шапку проекта
+require "db.php"; // подключаем файл для соединения с БД
+?>
+
+<div class="container mt-4">
+		<div class="row">
+			<div class="col">
+	   <!-- Форма регистрации -->
+		<h2>Форма регистрации</h2>
+		<form action="signup.php" method="post">
+			<input type="text" class="form-control" name="login" id="login" placeholder="Введите логин"><br>
+			<input type="email" class="form-control" name="email" id="email" placeholder="Введите Email"><br>
+			<input type="text" class="form-control" name="name" id="name" placeholder="Введите имя" required><br>
+			<input type="text" class="form-control" name="family" id="family" placeholder="Введите фамилию" required><br>
+			<input type="password" class="form-control" name="password" id="password" placeholder="Введите пароль"><br>
+			<input type="password" class="form-control" name="password_2" id="password_2" placeholder="Повторите пароль"><br>
+			<button class="btn btn-success" name="do_signup" type="submit">Зарегистрировать</button>
+		</form>
+		<br>
+		<p>Если вы зарегистрированы, тогда нажмите <a href="login.php">здесь</a>.</p>
+		<p>Вернуться на <a href="index.php">главную</a>.</p>
+			</div>
+		</div>
+	</div>
+<?php require __DIR__ . '/footer.php'; ?> <!-- Подключаем подвал проекта -->
