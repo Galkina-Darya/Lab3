9135179d81c488b41df8520c4ce9ab7886abb34b
Galkina-Darya <99248872+Galkina-Darya@users.noreply.github.com>
2022-11-17 12:07:14 +0300

    Update db.php

--- db.php
+++ db.php
@@ -10,3 +10,22 @@ if(!R::testConnection()) die('No DB connection!');
 
 session_start(); // Создаем сессию для авторизации
 ?>
+
+<html>
+    <head>
+        <title>php member system | registration form</title>
+    </head>
+
+    <body>
+        <h1>member system tutorial - register</h1> 
+            <a href="./index.php">Home</a> | 
+            <a href="./register.php">Register</a> | 
+            <a href="./login.php">Login</a> 
+        <hr />
+
+        <!-- error message code here -->
+
+        <!-- registration form html code here -->
+        
+    </body>
+</html>
