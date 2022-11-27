45a7b0eb7827c4303755a3762a2c18246c555c7d
Galkina-Darya <99248872+Galkina-Darya@users.noreply.github.com>
2022-11-27 23:06:40 +0300

    Update and rename index1.php to index.php

--- index1.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$connect = new mysqli("127.0.0.1", "root", "1234", "Lab_3_Galkina");
-if (!$connect) {
-  die("Error connection!);
-}
-
-session_start();
-int setcookie (string name [string name [, int 1 [, string Lab3 [, string 127.0.0.1 [, int 1]]]]])
-?>
